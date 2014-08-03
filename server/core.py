1676ef041c0613657e44a5da76956d44e3fed7eb
Yutong Zhao <yutong.zhao@stanford.edu>
2014-08-03 11:07:42 -0700

    Minor fixes

--- server/core.py
+++ server/core.py
@@ -1,64 +1,66 @@
-from threading import Thread, Event, Lock
+from __future__ import print_function
+
+from threading import Thread, Lock
 import time
 import zmq
-import signal
-import os
 import random
 
 from exit import should_shutdown
 
+
 class Core:
-	def __init__(self):
-		self.weights = None
-		self._weights_lock = Lock()
-		self.gradient = 'hehe'
-		self._gradient_lock = Lock()
-		self.context = zmq.Context()
-		self.id = str(random.randint(0, 200))
-		t = Thread(target=self._sync_weights)
-		t.start()
 
-	def _sync_weights(self):
-		socket = self.context.socket(zmq.REQ)
-		socket.connect('tcp://localhost:5739')
-		while not should_shutdown():
-			print 'syncing weights from server'
-			socket.send('i want weights!')
-			# use polling to make non-blocking
-			weights = int(socket.recv())
-			with self._weights_lock:
-				self.weights = weights
-				print 'weights updated successfully'
-			time.sleep(1)
+    def __init__(self):
+        self.weights = None
+        self._weights_lock = Lock()
+        self.gradient = 'hehe'
+        self._gradient_lock = Lock()
+        self.context = zmq.Context()
+        self.id = str(random.randint(0, 200))
+        t = Thread(target=self._pull_server_weights)
+        t.start()
 
-	def _push_gradient(self):
-		with self._gradient_lock:
-			print 'sending gradient...'
-			socket = self.context.socket(zmq.REQ)
-			socket.connect('tcp://localhost:5738')
-			socket.send("client_gradient: "+self.id)
-			# use polling to make non-blocking
-			message = socket.recv()
-			print 'gradient sent succesfully'
+    def _pull_server_weights(self):
+        socket = self.context.socket(zmq.REQ)
+        socket.connect('tcp://localhost:5739')
+        while not should_shutdown():
+            print('-----syncing weights from server')
+            socket.send('i want weights!')
+            # blocks
+            weights = int(socket.recv())
+            with self._weights_lock:
+                self.weights = weights
+                print('weights updated successfully')
+            time.sleep(10)
 
-	def async_push_gradient(self):
-		Thread(target=self._push_gradient).start()
+    def _push_gradient(self):
+        # if the shard server dies, this blocks the connection.
+        with self._gradient_lock:
+            socket = self.context.socket(zmq.REQ)
+            socket.connect('tcp://localhost:5738')
+            socket.send("client_gradient: "+self.gradient+" "+self.id)
+        # blocks
+        socket.recv()
+        print('gradient sent succesfully')
 
-	def compute_gradient(self):
-		with self._gradient_lock:
-			with self._weights_lock:
-				print 'computing expensive gradient'
-				time.sleep(5)
-				print 'finished computing expensive gradient'
+    def async_push_gradient(self):
+        Thread(target=self._push_gradient).start()
 
-	def apply_gradient(self):
-		with self._gradient_lock:
-			print 'applying gradient'
+    def compute_gradient(self):
+        with self._gradient_lock:
+            with self._weights_lock:
+                print('computing expensive gradient')
+                time.sleep(random.randint(1, 6))
+                print('finished computing expensive gradient')
+
+    def apply_gradient(self):
+        with self._gradient_lock:
+            print('applying gradient')
 
 c = Core()
 
 while not should_shutdown():
-	c.compute_gradient()
-	c.async_push_gradient()
-	c.apply_gradient()
-	time.sleep(1)
+    c.compute_gradient()
+    c.async_push_gradient()
+    c.apply_gradient()
+    time.sleep(1)
--- server/shard.py
+++ server/shard.py
@@ -1,45 +1,43 @@
-from threading import Thread, Event, Lock
+from threading import Thread, Lock
 import zmq
 
 from exit import should_shutdown
 
+
 class Shard:
-	def __init__(self):
-		self.weights = 1234
-		self._weights_lock = Lock()
-		self.context = zmq.Context()
-		Thread(target=self.send_core_server_weights).start()
-		Thread(target=self.apply_gradients).start()
 
-	def send_core_server_weights(self):
-		socket = self.context.socket(zmq.REP)
-		socket.bind('tcp://*:5739')
-		# use a polling mechanism to make it non-blocking
-		poller = zmq.Poller()
-		poller.register(socket, zmq.POLLIN)
-		while not should_shutdown():
-			socks = dict(poller.poll(100))
-			if socks:
-				message = socket.recv()
-				with self._weights_lock:
-					socket.send(str(self.weights))
+    def __init__(self):
+        self.weights = 1234
+        self._weights_lock = Lock()
+        self.context = zmq.Context()
+        Thread(target=self.send_weights_to_core).start()
+        Thread(target=self.apply_gradients).start()
 
-	def apply_gradients(self):
-		socket = self.context.socket(zmq.REP)
-		socket.bind('tcp://*:5738')
-		poller = zmq.Poller()
-		poller.register(socket, zmq.POLLIN)
-		# use a polling mechanism to make it non-blocking
-		while not should_shutdown():
-			socks = dict(poller.poll(100))
-			if socks:
-				if socks.get(socket) == zmq.POLLIN:
-					message = socket.recv()
-					with self._weights_lock:
-						print('server received gradient:', message)
-						socket.send('OK')
+    def send_weights_to_core(self):
+        # the core can request new copies of parameters from the server
+        socket = self.context.socket(zmq.REP)
+        socket.bind('tcp://*:5739')
+        poller = zmq.Poller()
+        poller.register(socket, zmq.POLLIN)
+        while not should_shutdown():
+            if poller.poll(100):
+                socket.recv()
+                with self._weights_lock:
+                    socket.send(str(self.weights))
+
+    def apply_gradients(self):
+        socket = self.context.socket(zmq.REP)
+        socket.bind('tcp://*:5738')
+        poller = zmq.Poller()
+        poller.register(socket, zmq.POLLIN)
+        while not should_shutdown():
+            if poller.poll(100):
+                message = socket.recv()
+                with self._weights_lock:
+                    print('server received gradient:', message)
+                    socket.send('OK')
 
 s = Shard()
 
 while not should_shutdown():
-	pass
+    pass
